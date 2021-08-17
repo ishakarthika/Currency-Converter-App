74af9d876be8d157abc6157bb5d2d9ad627ee1a8
ishakarthika <ishakarthika2000@gmail.com>
2021-08-17 13:09:58 +0530

    Added title to the app

--- script.js
+++ script.js
@@ -5,7 +5,11 @@ window.onload = function() {
  
     if(!logInUser || logInUser == '') {
         myModal.show();
-    }
+        
+    } else{
+        document.getElementById("notlogin").style.display="none";
+        document.getElementById("login-text").innerHTML="Hi "+logInUser+ ", welcome back to Currency Converter";
+    } 
     var userNameField = document.getElementById("formGroupExampleInput2");
 
     userNameField.addEventListener('keyup', function(event){
@@ -24,4 +28,9 @@ function login() {
         localStorage.setItem('user', user);
         myModal.hide();
     }
+    document.getElementById("notlogin").style.display="none";
+    document.getElementById("login-text").innerHTML="Hi "+user+ ", welcome back to Currency Converter";
 }
+
+
+
