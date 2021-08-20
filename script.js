6039cae0b99a079d0f1afaf49dd8ea644f20b968
ishakarthika <ishakarthika2000@gmail.com>
2021-08-20 14:19:08 +0530

    Loaded list

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 var myModal
+var countries;
+var fromList;
+// load cities
 window.onload = function() {
     myModal = new bootstrap.Modal(document.getElementById('loginModal'))
     var logInUser=localStorage.getItem('user');
@@ -20,7 +23,7 @@ window.onload = function() {
             userNameField.classList.remove('is-invalid');
             userNameField.classList.add('is-valid');
         }
-    });
+    }); 
 }
 function login() {
     var user=document.getElementById("formGroupExampleInput2").value;
@@ -31,6 +34,46 @@ function login() {
     document.getElementById("notlogin").style.display="none";
     document.getElementById("login-text").innerHTML="Hi "+user+ ", welcome back to Currency Converter";
 }
+ // code to generate list dropdown;
+function createFromDropdown(list) {
+    countries.forEach(function(country) {
+        console.log(country);
+        var code;
+        var option = document.createElement('li');
+        var flag = document.createElement('span');
+        option.value=country.currency_code;
+        flag.classList.add('currency-flag')
+        if(country.currency_code) {
+         code = 'currency-flag-'+country.currency_code.toLowerCase();
+        } else {
+         code = 'currency-flag-'+country.currency_code;
+        }
+        flag.classList.add(code);
+        option.innerHTML = country.country + ' ('+ country.currency_code+')';
+        option.prepend(flag);
+       
+        fromList.appendChild(option);
+    })
+}
+// loading country list
+$(document).ready(function() {
+    $.getJSON('./assets/country-json/src/country-by-currency-code.json', function(data) {
+        console.log(data);
+        countries = data;
+        // from country dropdown code
+        fromList = document.getElementById('from-country-list');
+        createFromDropdown(fromList);
+
+        fromList.addEventListener('click', function(event) {
+            console.log('e', event);
+            document.getElementById('from-cc-selected').innerHTML = event.target.textContent;
+        })
+
+        // to country dropdown code;
+
+    });
+})
+
 
 
 
